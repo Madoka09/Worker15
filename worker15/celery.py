94f4a85c2fbb465700bfe20e5ad917b6c384bf3c
Madoka09 <personal.galvan.francisco@gmail.com>
2021-03-06 00:16:44 -0600

    cambios

--- worker15/celery.py
+++ worker15/celery.py
@@ -15,27 +15,10 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 app.conf.beat_schedule = {
     'Check_WS_24H': {
         'task': 'tasker.views.check_dates',
-        'schedule': 35,
-        #'args': request
+        'schedule': crontab(hour=1, minute=0)
     },
 }
 #app.conf.timezone = 'UTC'
-
-'''
-@app.on_after_configure.connect 
-def setup_periodic_tasks(sender, **kwargs):
-    # Calls test('hello') every 10 seconds.
-    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
-
-    # Calls test('world') every 30 seconds
-    sender.add_periodic_task(30.0, test.s('world'), expires=10)
-
-    # Executes every Monday morning at 7:30 a.m.
-    sender.add_periodic_task(
-        crontab(hour=7, minute=30, day_of_week=1),
-        test.s('Happy Mondays!'),
-    )
-'''
 
 @app.task(bind=True)
 def debug_task(self):
